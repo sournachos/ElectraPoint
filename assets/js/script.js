0f37fc52c3f9cce6460ca68614f47fc953bf7292
sournachos <joserivera18100@gmail.com>
2021-04-06 10:15:00 -0500

    JS edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
-let city = document.querySelector('.userInp').value
+// let city = document.querySelector('.userInp').value
 let btn = document.querySelector('.btn')
 
-fetch("https://api.openchargemap.io/v3/poi/?output=json&countrycode=US&maxresults=5")
+let apiKey = '053d958f-ba09-46ab-a82c-205e08310970'
+
+fetch("https://api.openchargemap.io/v3/poi/?output=json&countrycode=US&maxresults=5", {
+
+})
 .then(res =>{return res.json()})
 // .then(data => console.log(data))
 // .catch(error => console.log('Whoops wrong turn!'))
 
-// btn.addEventListener('click', function(){
-// console.log(data)
+btn.addEventListener('click', function(){
+console.log(res)
 
-// })
+})
 
 
 
