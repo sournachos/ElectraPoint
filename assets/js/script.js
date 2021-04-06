11ef55f03f1744aadd37abfd21ab8a477c64c4b9
sournachos <joserivera18100@gmail.com>
2021-04-06 13:20:35 -0500

    Adding an HTML file for the map page and another HTML file for the landing page

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,13 @@ let btn = document.querySelector('.btn')
 
 let apiKey = '053d958f-ba09-46ab-a82c-205e08310970'
 
-fetch("https://api.openchargemap.io/v3/poi/?output=json&countrycode=US&maxresults=5", {
-
-})
+fetch("https://api.openchargemap.io/v3/poi/?output=json&countrycode=US&maxresults=5" + '&key=' + apiKey)
 .then(res =>{return res.json()})
-// .then(data => console.log(data))
-// .catch(error => console.log('Whoops wrong turn!'))
+.then(data => console.log(data))
+.catch(error => console.log('Whoops wrong turn!'))
 
 btn.addEventListener('click', function(){
-console.log(res)
+console.log(data)
 
 })
 
