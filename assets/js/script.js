8bd72a928455b4d4c2b7291ecd6a848f92e1be48
Jose Rivera <80435061+sournachos@users.noreply.github.com>
2021-04-08 12:37:05 -0500

    Updating JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,6 @@ btn.onclick = function() {
   span.onclick = function() {
     modal.style.display = "none";
   }
-
     window.onclick = function(event) {
       if (event.target == modal) {
         modal.style.display = "none";
